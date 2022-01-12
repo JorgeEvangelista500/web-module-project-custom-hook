954e33c59dd94e25409c4eacc4988ac0aba53131
Jorge <jorgeevangelista500@gmail.com>
2022-01-12 17:41:33 -0500

    finished local storage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = () =>{
-    const [darkMode, setDarkMode] = useState(false);
-    
+    // const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useLocalStorage('darkmode', false)
+
     return ([darkMode, setDarkMode])
 }
 
